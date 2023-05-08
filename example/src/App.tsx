be75f0e86b76c9378f2ffc427460e5d979d06e06
Maximiliano Correa <jmaxicorrea@gmail.com>
2023-05-08 01:01:38 -0300

    Create MxListItem Component!

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-
 import { ExampleComponent } from 'mx-list-item'
-import 'mx-list-item/dist/index.css'
 
 const App = () => {
   return <ExampleComponent text="Create React Library Example 😄" />
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import { makeStyles } from '@material-ui/core'
+import { ListItem } from '@material-ui/core'
 
-interface Props {
-  text: string
-}
+const useStyles = makeStyles({
+  root: {
+    color: '#0c1932 !important',
+    zIndex: 99,
+    float: 'left',
+    position: 'relative',
+    display: 'block',
+    width: 'auto !important',
+    margin: '0px !important',
+    padding: '0px !important',
+    border: 'none !important'
+  }
+})
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const PrepagaListItem: React.FC = (props) => {
+  const classes = useStyles()
+  return (
+    <ListItem
+      classes={{
+        root: classes.root
+      }}
+    >
+      {props.children}
+    </ListItem>
+  )
 }
