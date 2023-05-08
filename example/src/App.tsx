aa445853a8ce820ab6cfbebb1c09ea4bc1be7453
Maximiliano Correa <jmaxicorrea@gmail.com>
2023-05-07 21:07:21 -0300

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'mx-list-item'
+import 'mx-list-item/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
